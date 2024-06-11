5e4072849f48eb8b96a5ceab2f751ccf773761a8
Sergio Cid <sergio.g.cid.m@gmail.com>
2024-06-11 14:11:43 +0200

    fixing errors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.static('dist'))
 morgan.token("post-data", function (req, res) {
     if (req.method === "POST") {
         const { name, number } = req.body;
